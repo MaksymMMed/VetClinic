f29f7367df1053b0808affa4a6cfa0cf1136863c
Maksym Mediuh <maksim2003mmm15@gmail.com>
2023-07-18 15:55:06 +0300

    Add entities

---
+++ VetClinic.DAL/Entities/Animal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetClinic.DAL.Entities
+{
+    public class Animal:BaseEntity
+    {
+        public string Name { get;set; }
+        public int Age { get;set; }
+        public string Description { get; set; }
+        public Sex AnimalSex { get;set; }
+        public Kind AnimalKind { get;set; }
+        public int OwnerId { get;set; }
+        public Customer Owner { get;set; }
+        public List<Reception>? Receptions { get; set; }
+
+        public enum Sex
+        {
+              Male,
+              Female
+        }
+
+        public enum Kind
+        {
+            Dog,
+            Cat,
+            Mouse,
+            Bunny,
+            Parrot,
+            Hamster,
+            Turtle,
+            Lizard,
+            Other
+        }
+    }
+}
---
+++ VetClinic.DAL/Entities/Reception.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetClinic.DAL.Entities
+{
+    public class Reception:BaseEntity
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool IsOver { get; set; }
+        public int DoctorId { get; set; }
+        public int AnimalId { get; set; }
+        public Doctor Doctor { get; set; }
+        public Animal Animal { get; set; }
+        public List<ProcedureType> Procedures { get; set; }
+
+
+        public enum ProcedureType
+        {
+            PhysicalExamination,
+            Vaccination,
+            WoundDisinfection,
+            Spaying,
+            DenthalCheck,
+            LabTests,
+            Radiology,
+            Ultrasonography,
+            Electocardiodiagram,
+            SurgicalProcedures
+        }
+
+    }
+}
