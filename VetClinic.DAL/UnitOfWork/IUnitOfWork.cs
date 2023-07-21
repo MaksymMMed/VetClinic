4f657f8f12c9ef59a9fb2e6a1454709886cc17ab
Maksym Mediuh <maksim2003mmm15@gmail.com>
2023-07-21 22:33:54 +0300

    Add Unit of Work

---
+++ VetClinic.DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VetClinic.DAL.Entities;
+using VetClinic.DAL.Repository.Interfaces;
+
+namespace VetClinic.DAL.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        UserManager<BaseAccount> UserManager { get; }
+
+        SignInManager<BaseAccount> SignInManager { get; }
+
+        IAnimalRepository AnimalRepository { get; }
+        IAppointmentRepository AppointmentRepository { get; }
+        Task SaveChangesAsync();
+    }
+}
---
+++ VetClinic.DAL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VetClinic.DAL.Entities;
+using VetClinic.DAL.Repository.Interfaces;
+
+namespace VetClinic.DAL.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDatabaseContext Context;
+
+        public UnitOfWork(AppDatabaseContext context, UserManager<BaseAccount> userManager,
+            SignInManager<BaseAccount> signInManager, IAnimalRepository animalRepository,
+            IAppointmentRepository appointmentRepository)
+        {
+            Context = context;
+            UserManager = userManager;
+            SignInManager = signInManager;
+            AnimalRepository = animalRepository;
+            AppointmentRepository = appointmentRepository;
+        }
+
+        public UserManager<BaseAccount> UserManager { get; }
+        public SignInManager<BaseAccount> SignInManager { get; }
+        public IAnimalRepository AnimalRepository { get; }
+        public IAppointmentRepository AppointmentRepository { get; }
+
+        public async Task SaveChangesAsync()
+        {
+            await Context.SaveChangesAsync();
+        }
+    }
+}
